8d0e76343bb882ef98a3e36bf696bcd6a3288d9d
Masataka Aizawa <masatakaaizawa@Masatakas-MacBook-Air.local>
2023-08-08 11:44:41 +0800

    added test tutorial

---
+++ tests/run.sh
@@ -0,0 +1,1 @@
+python run_sampling.py --n_process 3 --config ./paras/mcmc_config.dat --initial_para ./paras/AS209_paradic.dat --prior ./paras/prior.dat --visfile ./vis_data/AS209_continuum_averaged.vis.npz
